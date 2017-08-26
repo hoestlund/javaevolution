1573c71badd08eb26de9836046240618edb062a1
Oskar Enmalm <oskarenmalm@gmail.com>
2017-08-27 00:59:31 +0200

    %
    Cleaning up

--- Ch3/src/Main.java
+++ Ch3/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 //Will execute the other classes that make up the questions from Downey's book
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		
 		UserFeedback one = new UserFeedback();
 		//one.ReadingInput();
@@ -11,6 +12,14 @@ public class Main {
 		
 		PowerPro eh = new PowerPro(); //eh is exponential help 
 		eh.UserInterface();
+=======
+		// TODO Auto-generated method stub
+		UserFeedback one = new UserFeedback();
+		one.ReadingInput();
+		
+		Notes myExperiments = new Notes();
+		myExperiments.ConvertingDoubleToInt();
+>>>>>>> 3a7666e1158a3d15bd043009565bbdec35249ac0
 	}
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javaevolution
+This is quite messsy and is for personal use right now to help me gain some sense of achievement. Hoping to see some nice project in what I am making in this project.
+Will be guided through my learning with the courses "Introduction to Java and OOP" and "Graphical object-orientated and Internet programming in Java"
