17305649da64eb8f658fc7fa0460541220f11753
Oskar Enmalm <oskarenmalm@gmail.com>
2017-08-26 20:58:11 +0200

    Updated Chapter 3

--- Ch3/src/Main.java
+++ Ch3/src/Main.java
@@ -1,9 +1,10 @@
 
 public class Main {
-
+//Will execute the other classes that make up the questions from Downey's book
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		UserFeedback one = new UserFeedback();
+		one.ReadingInput();
 	}
 
 }
---
+++ Ch3/src/Notes.java
@@ -0,0 +1,18 @@
+
+public class Notes {
+	//Converting can be done by creating another variable in the data type wanted, example:
+	
+	
+	public void ConvertingDoubleToInt() {
+	
+		double pi = 3.14159265358;
+		int pIndiana = (int) pi;
+		System.out.println("Some decimals of pi; " + pi);
+		System.out.println("No decimals of pi; " + pIndiana);
+		System.out.println("This is pi"); 
+	}
+	
+	
+	
+	
+}
