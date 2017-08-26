85b75a4208b2ff07e8be5df7bb83a32f54f5e85f
Oskar Enmalm <oskarenmalm@gmail.com>
2017-08-26 23:38:55 +0200

    Add files via upload

--- Ch3/src/Main.java
+++ Ch3/src/Main.java
@@ -1,9 +1,13 @@
 
 public class Main {
-
+//Will execute the other classes that make up the questions from Downey's book
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		UserFeedback one = new UserFeedback();
+		one.ReadingInput();
+		
+		Notes myExperiments = new Notes();
+		myExperiments.ConvertingDoubleToInt();
 	}
 
 }
---
+++ Ch3/src/UserFeedback.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserFeedback {
+
+	Scanner Reader = new Scanner(System.in);
+	
+	
+	public void ReadingInput() {
+	System.out.println("What is your name?");
+	String name = Reader.nextLine(); //reads next line and assigns 
+	System.out.println("Hello " + name);
+	
+	
+	}
+}
