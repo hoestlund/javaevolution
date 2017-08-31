0ca55854ceec05f2fcc662d4a7ea96ec49a895e6
Oskar Enmalm <oskarenmalm@gmail.com>
2017-08-31 22:35:59 +0200

    Thurs
    Improving rudamentary little song. Wanting to use elements jar but failing slightly.
    Still a bit insecure about classpaths and using external packages

--- Ch5/src/Main.java
+++ Ch5/src/Main.java
@@ -3,10 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//BottlesOfBeerOnTheWall Song = new BottlesOfBeerOnTheWall();
+		//Song.BeerBottles();
+		//Read up about assessing methods in a static way,  
+		//https://stackoverflow.com/questions/13537443/java-accessing-static-method-properly
+		BottlesOfBeerOnTheWall.BeerBottles();
 		
-		BottlesOfBeerOnTheWall song = new BottlesOfBeerOnTheWall();
-		//Read up about assessing static.  
-		song.CheerySong();
+		
+		
+		
+		
+		
 
 	}
 
