b9e6753eec2de186d8331096761c14ad21eafcaa
Oskar Enmalm <oskarenmalm@gmail.com>
2017-09-04 20:46:42 +0200

    Mon
    Reading through Ch6, using a lot of math functions and looking into recurssion and overloading methods.

---
+++ Ch6/Recursion.java
@@ -0,0 +1,14 @@
+
+public class Recursion {
+
+	public static int factorial(int n) {
+		if(n==0) {
+			return 1;
+		} else {
+			int recurse = factorial(n-1);
+			int result = n * recurse;
+			return result;
+			
+		}
+	}
+}
