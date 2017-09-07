17b8dad41f8c20fcf87907bacd9138fd6e8e5f02
Oskar Enmalm <oskarenmalm@gmail.com>
2017-09-07 23:29:28 +0200

    .
    Wanting to start chapter 7 with Intellij, having some problem with using external jar files.
    
    Will complete chapter 6 exercises and then try to make the orientation easier i.e. landing displayed on git being the classes themselves instead of having to navigate down.

---
+++ Ch6/Exercises.java
@@ -0,0 +1,11 @@
+
+public class Exercises {
+	
+	public static Boolean isDivisible(int n, int m) {
+		if(!(n%m==0)) {
+			return false; } 
+		else return true;
+		
+	}
+
+}
--- Ch6/Main.java
+++ Ch6/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(Recursion.factorial(4));
+		//System.out.println(Recursion.factorial(4)); test case
+		//System.out.println(Exercises.isDivisible(4, 2)); test case
+		
 
 	}
 
