56af20d1fe3034b89cec6efcfdc4e9214fa27a55
Oskar Enmalm <oskarenmalm@gmail.com>
2017-09-26 16:30:46 +0200

    8.6 Explanation

--- Ch8 - Strings and things/Main.java
+++ Ch8 - Strings and things/Main.java
@@ -5,12 +5,13 @@ public class Main {
         //®Exercises.reverse("This is a string backwards");
         //8.2 is not a coding exercise
         //Exercises.countLetters("helen", 'e');
-        Exercises.reverse("This is my reverse text");
-        Exercises.checkA("banana", 'a');
-        Exercises.countLetters("banana", 'a');
-        Exercises.test();
-        Exercises.puzzler();
-        System.out.println(mystery("Allen"));
+//        Exercises.reverse("This is my reverse text");
+//        Exercises.checkA("banana", 'a');
+//        Exercises.countLetters("banana", 'a');
+//        Exercises.test();
+//        Exercises.puzzler();
+        //Mystery takes a string and numbers each char. It prints a new line with the index and the char at that index, then prints the string backwards
+        System.out.println(Mystery.mystery("Allen"));
 
 
     }//End of main method
