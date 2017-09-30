813ecca6dd938fdfd1684ea8d0e9624f8098e8e5
Oskar Enmalm <oskarenmalm@gmail.com>
2017-09-30 16:17:19 +0200

    Practical powers with big
    Many encryption techniques depend on the ability to raise large integers to an integer power. Here is a method that implements a (reasonably) fast technique for integer exponentiation.
    
    This method was given and goal was to apply BigInteger to make it work.

--- Ch9 - Mutable Objects/src/Big.java
+++ Ch9 - Mutable Objects/src/Big.java
@@ -27,4 +27,19 @@ public class Big {
         }
 
     }
+
+
+//TODO USE BIG
+    public static BigInteger pow(int x, int n) {
+        if (n == 0) return BigInteger.valueOf(1);
+        // find x to the n/2 recursively
+        BigInteger t = pow(x, n/2);
+        // if n is even, the result is t squared
+        // if n is odd, the result is t squared times x
+        if (n%2 == 0) {
+            return t.multiply(t); //value of long in BigInteger could not be applied
+        } else {
+            return t.multiply(t).multiply(BigInteger.valueOf(x));
+        }
+    }
 }
