6b1b1b094714d858cd1f37db715e5ee476a22505
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-02 20:42:43 +0200

    Date Object
    It's my birthday and you're invited to see it being created and printed twice!

--- Ch11 - Create your own objects/src/Main.java
+++ Ch11 - Create your own objects/src/Main.java
@@ -2,9 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //Tile random = new Tile();
+
         //random.testTile(random);
         // This feels so wrong: Create an object to call a method that create an object with parameters of object
+        //Tile random = new Tile('z', 10);
+        //random.printTile(random);
+
+//Another quick practice. Because the parameter is a class it means the method relies on class creation.
+// Therefore the method can not be accessed in a static way e.g. Date.printBirthday
+        Date birthday = new Date(1994, 02, 27);
+        birthday.printBirthday(birthday);
+
+        Date longBirthday = new Date();
+        longBirthday.year = 1994;
+        longBirthday.month = 02;
+        longBirthday.date = 27;
+        longBirthday.printBirthday(longBirthday);
 
     }
 }
