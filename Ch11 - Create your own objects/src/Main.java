a1fe8ac1ef929658e377d95dde18dd0f48fa3804
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-02 20:20:06 +0200

    Objects!
    First steps into objects.
    Learning about constructors.

---
+++ Ch11 - Create your own objects/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Tile random = new Tile();
+        //random.testTile(random);
+        // This feels so wrong: Create an object to call a method that create an object with parameters of object
+
+    }
+}
